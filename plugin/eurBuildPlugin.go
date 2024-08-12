9267d18796366e9352e916c7c2b10774c6289ad3
shishupei <shishupei10@163.com>
2024-08-12 10:53:21 +0800

    写点注释

--- plugin/eurBuildPlugin.go
+++ plugin/eurBuildPlugin.go
@@ -1,6 +1,7 @@
 package plugin
 
 import (
+	"encoding/json"
 	"github.com/IBM/sarama"
 	kfklib "github.com/opensourceways/kafka-lib/agent"
 	"github.com/opensourceways/message-collect/common/kafka"
@@ -26,6 +27,9 @@ func (h EurGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error {
 	return nil
 }
 
+const start_topic = "org.openEuler.prod.eur.build.start"
+const end_topic = "org.openEuler.prod.eur.build.end"
+
 /*
 *
 
@@ -33,13 +37,46 @@ func (h EurGroupHandler) Cleanup(_ sarama.ConsumerGroupSession) error {
 */
 func (h EurGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for message := range claim.Messages() {
-		err := kfklib.Publish(config.EurBuildConfigInstance.Publish, nil, message.Value)
-		if err != nil {
-			logrus.Error(err)
-			return err
+		var raw EurBuildMessageRaw
+		msgBodyErr := json.Unmarshal(message.Value, &raw)
+		if msgBodyErr != nil {
+			return msgBodyErr
 		}
-		session.MarkMessage(message, "")
-		logrus.Info("send eur build success")
+		if raw.Topic == start_topic || raw.Topic == end_topic {
+			err := kfklib.Publish(config.EurBuildConfigInstance.Publish, nil, message.Value)
+			if err != nil {
+				logrus.Error(err)
+				return err
+			}
+			session.MarkMessage(message, "")
+			logrus.Info("send eur build success")
+		} else {
+			logrus.Info("范围外的topic" + raw.Topic)
+
+		}
 	}
 	return nil
+}
+
+type EurBuildMessageRaw struct {
+	Body struct {
+		User    string      `json:"user"`
+		Copr    string      `json:"copr"`
+		Owner   string      `json:"owner"`
+		Pkg     interface{} `json:"pkg"`
+		Build   int         `json:"build"`
+		Chroot  string      `json:"chroot"`
+		Version interface{} `json:"version"`
+		Status  int         `json:"status"`
+		IP      string      `json:"ip"`
+		Who     string      `json:"who"`
+		Pid     int         `json:"pid"`
+		What    string      `json:"what"`
+	} `json:"body"`
+	Headers struct {
+		OpenEulerMessagingSchema string `json:"openEuler_messaging_schema"`
+		SentAt                   string `json:"sent-at"`
+	} `json:"headers"`
+	ID    string `json:"id"`
+	Topic string `json:"topic"`
 }
