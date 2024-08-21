58d641d13d5b8f6419274c0cc03a7da9bb25b3d3
shishupei <shishupei10@163.com>
2024-08-21 10:07:04 +0800

    接入测试环境

--- plugin/openEulerMeetingPlugin.go
+++ plugin/openEulerMeetingPlugin.go
@@ -53,18 +53,27 @@ func (h OpenEulerMeetingHandler) ConsumeClaim(session sarama.ConsumerGroupSessio
 type OpenEulerMeetingRaw struct {
 	Action string `json:"action"`
 	Msg    struct {
-		Topic     string      `json:"topic"`
-		Platform  interface{} `json:"mplatform"`
-		Sponsor   string      `json:"sponsor"`
-		GroupName string      `json:"group_name"`
-		GroupId   interface{} `json:"group_id"`
-		Date      string      `json:"date"`
-		Start     string      `json:"start"`
-		End       string      `json:"end"`
-		Etherpad  string      `json:"etherpad"`
-		Agenda    string      `json:"agenda"`
-		EmailList string      `json:"emaillist"`
-		Record    string      `json:"record"`
-		JoinUrl   string      `json:"join_url"`
+		Id        int    `json:"id"`
+		Topic     string `json:"topic"`
+		Community string `json:"community"`
+		GroupName string `json:"group_name"`
+		Sponsor   string `json:"sponsor"`
+		Date      string `json:"date"`
+		Start     string `json:"start"`
+		End       string `json:"end"`
+		Duration  string `json:"duration"`
+		Agenda    string `json:"agenda"`
+		Etherpad  string `json:"etherpad"`
+		Emaillist string `json:"emaillist"`
+		HostId    string `json:"host_id"`
+		Mid       string `json:"mid"`
+		Mmid      string `json:"mmid"`
+		JoinUrl   string `json:"join_url"`
+		IsDelete  int    `json:"is_delete"`
+		StartUrl  string `json:"start_url"`
+		Timezone  string `json:"timezone"`
+		User      int    `json:"user"`
+		Group     int    `json:"group"`
+		Mplatform string `json:"mplatform"`
 	} `json:"msg"`
 }
